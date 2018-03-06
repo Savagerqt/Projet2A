0438ac42064869b0eadabefb857e4c4353d02da9
Tanguy VIVIER <tanguy.viv@gmail.com>
2018-03-06 15:43:41 +0100

    PPT + Photos
    Powerpoint et photos des simulations

--- Python/Polygon_Optimization.py
+++ Python/Polygon_Optimization.py
@@ -5,51 +5,51 @@
 =================================================================================
 """
 
-from Vertex import * 
-import matlab.engine   
+from Vertex import *
+import matlab.engine
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 
 
 
 # ===============================================================================
-#                             Best Value of a vertex  
+#                             Best Value of a vertex
 # ===============================================================================
 
-#   On cherche la position du sommet i qui maximise la fonctionnelle de forme 
-#   la fonction prend en paramètres : 
+#   On cherche la position du sommet i qui maximise la fonctionnelle de forme
+#   la fonction prend en paramètres :
 #
-#   - initValue : la valeur de l'intégrale du polygone initial (pour optimiser 
+#   - initValue : la valeur de l'intégrale du polygone initial (pour optimiser
 #     et ne pas avoir à le calculer à chaque fois)
 #
-#   - dl : la longueur du pas 
+#   - dl : la longueur du pas
 #
-#   - i : le numéro du sommet 
+#   - i : le numéro du sommet
 #
-#   - nbTest : le nombre de valeur testée de part et d'autre du sommet, en tout 
+#   - nbTest : le nombre de valeur testée de part et d'autre du sommet, en tout
 #     2*nbTest valeurs sont testées sur chaque sommet
 #
 #   - eng : le moteur matlab
 #
 
-def bestValue(polygon, initValue, i, nbTest, dl, eng) :  
+def bestValue(polygon, initValue, i, nbTest, dl, eng) :
 
-    #   On stocke les valeurs des intégrales liées à chacun des déplacements 
-    #   dans une liste 
-    #   right correspond aux déplacements à droite du point 
-    #   left correspond aux déplacement à gauche du point 
+    #   On stocke les valeurs des intégrales liées à chacun des déplacements
+    #   dans une liste
+    #   right correspond aux déplacements à droite du point
+    #   left correspond aux déplacement à gauche du point
 
     left = [polygon.valueIntegral(i, -j * dl, eng) for j in range(nbTest)]
     right = [polygon.valueIntegral(i, j * dl, eng) for j in range(nbTest)]
     L = np.array(left + [initValue] + right)
 
-    #   On cherche le maximum dans cette liste, puis on trouve l'index de ce 
+    #   On cherche le maximum dans cette liste, puis on trouve l'index de ce
     #   maximum, qu'on appelle indexMax
-    indexMax = np.argmax(L)    
+    indexMax = np.argmax(L)
 
     #   Le résulat retourné contient la valeur maximum de l'intégrale,
     #   et le déplacement associé à celui-ci
-    return [L[indexMax], (indexMax - nbTest) * dl] 
+    return [L[indexMax], (indexMax - nbTest) * dl]
 
 
 
@@ -57,115 +57,115 @@ def bestValue(polygon, initValue, i, nbTest, dl, eng) :
 
 
 
-def bestValueOS(polygon, initValue, i, nbTest, dl, eng) :  
+def bestValueOS(polygon, initValue, i, nbTest, dl, eng) :
     #    OS : Odd Symetrical
-    #   On stocke les valeurs des intégrales liées à chacun des déplacements 
-    #   dans une liste 
-    #   right correspond aux déplacements à droite du point 
-    #   left correspond aux déplacement à gauche du point 
+    #   On stocke les valeurs des intégrales liées à chacun des déplacements
+    #   dans une liste
+    #   right correspond aux déplacements à droite du point
+    #   left correspond aux déplacement à gauche du point
 
     left = [polygon.valueIntegral(i, -j * dl, eng) for j in range(nbTest)]
     right = [polygon.valueIntegral(i, j * dl, eng) for j in range(nbTest)]
     L = np.array(left + [initValue] + right)
 
-    #   On cherche le maximum dans cette liste, puis on trouve l'index de ce 
+    #   On cherche le maximum dans cette liste, puis on trouve l'index de ce
     #   maximum, qu'on appelle indexMax
-    indexMax = np.argmax(L)    
+    indexMax = np.argmax(L)
 
     #   Le résulat retourné contient la valeur maximum de l'intégrale,
     #   et le déplacement associé à celui-ci
-    return [L[indexMax], (indexMax - nbTest) * dl] 
+    return [L[indexMax], (indexMax - nbTest) * dl]
 
 
 
 
 
 # ===============================================================================
-#                              Naive Mainloop 
+#                              Naive Mainloop
 # ===============================================================================
 
-#   Cette boucle naïve se contente de parcourir les sommets pour trouver le 
-#   meilleur déplacement possible 
+#   Cette boucle naïve se contente de parcourir les sommets pour trouver le
+#   meilleur déplacement possible
 
 
-def naiveMainloop(polygon, dl, nbTest, nbIteration, values, eng) : 
-    #   Conservation des données de la temperature moyenne pour chaque itération 
-    #   values est une liste vide destinée à conserver les valeurs de l'intégrale 
+def naiveMainloop(polygon, dl, nbTest, nbIteration, values, eng) :
+    #   Conservation des données de la temperature moyenne pour chaque itération
+    #   values est une liste vide destinée à conserver les valeurs de l'intégrale
 
-    if nbIteration == 0 : 
+    if nbIteration == 0 :
         print("Fin de la simulation")
         return 0
 
     initValue = polygon.valueIntegral(0,0,eng)
 
-    #   On cherche le petit déplacement qui maximise notre fonctionnelle 
-    #   de forme lors d'une itération de l'algorithme 
+    #   On cherche le petit déplacement qui maximise notre fonctionnelle
+    #   de forme lors d'une itération de l'algorithme
     #   On examine chacun des sommets indépendamment
 
     max = [0,0]                                                     # Initialisation du maximum
-    rank = 0 
-    for i in range(2, polygon.N) : 
+    rank = 0
+    for i in range(2, polygon.N) :
         val = bestValue(polygon, initValue, i, nbTest, dl, eng)
         if val[0] > max[0] :
             max = val
-            rank = i 
+            rank = i
 
-    #   Si la valeur maximum est atteinte pour un déplacement nul, 
-    #   on arrête la simulation 
-    if max[1] == 0 : 
-        print("Il reste " + str(nbIteration) + " itérations") 
-        return 0 
+    #   Si la valeur maximum est atteinte pour un déplacement nul,
+    #   on arrête la simulation
+    if max[1] == 0 :
+        print("Il reste " + str(nbIteration) + " itérations")
+        return 0
 
-    # Sinon on bouge un sommet 
-    polygon.move(rank, max[1]) 
+    # Sinon on bouge un sommet
+    polygon.move(rank, max[1])
     values.append(max[0])
 
-    # Appel récursive de la fonction 
-    naiveMainloop(polygon, dl, nbTest, nbIteration - 1, values, eng) 
+    # Appel récursive de la fonction
+    naiveMainloop(polygon, dl, nbTest, nbIteration - 1, values, eng)
 
 
 
 # ===============================================================================
-#                          Mainloop with refinement 
+#                          Mainloop with refinement
 # ===============================================================================
 
 #   Il s'agit plus ou moins de la même boucle que la précédente, sauf que cette fois
-#   ci, l'algorithme rafine le pas quand la polygone a été suffisamment optimisé 
+#   ci, l'algorithme rafine le pas quand la polygone a été suffisamment optimisé
 #   pour un pas donné
 
-def refineMainloop(polygon, dl, nbIteration, eng) : 
+def refineMainloop(polygon, dl, nbIteration, eng) :
 
-    if nbIteration == 0 or dl < 0.001 : 
+    if nbIteration == 0 or dl < 0.001 :
         print("Fin de la simulation")
         return 0
 
     initValue = polygon.valueIntegral(0,0,eng)
 
-    #   On cherche le petit déplacement qui maximise notre fonctionnelle 
-    #   de forme lors d'une itération de l'algorithme 
+    #   On cherche le petit déplacement qui maximise notre fonctionnelle
+    #   de forme lors d'une itération de l'algorithme
     #   On examine chacun des sommets indépendamment
 
     max = [0,0]                                                     # Initialisation du maximum
-    rank = 0 
-    for i in range(2, polygon.N) : 
+    rank = 0
+    for i in range(2, polygon.N) :
         val = bestValue(polygon, initValue, i, dl, eng)
         if val[0] > max[0] :
             max = val
-            rank = i 
+            rank = i
 
     #   Si le polygone ne bouge plus, on raffine le pas
-    #   On ne prend pas non plus un pas trop petit, car 
-    #   cela sera inutile 
+    #   On ne prend pas non plus un pas trop petit, car
+    #   cela sera inutile
 
-    if max[1] == 0 :  
+    if max[1] == 0 :
         print("Raffinement du pas" + str(dl/2))
         refineMainloop(polygon, dl/2, nbIteration - 1, eng)
 
-    # Sinon on bouge un sommet 
-    polygon.move(rank, max[1]) 
+    # Sinon on bouge un sommet
+    polygon.move(rank, max[1])
 
-    # Appel récursive de la fonction 
-    refineMainloop(polygon, dl, nbIteration - 1, eng) 
+    # Appel récursive de la fonction
+    refineMainloop(polygon, dl, nbIteration - 1, eng)
 
 
 
@@ -175,62 +175,47 @@ def refineMainloop(polygon, dl, nbIteration, eng) :
 # ===============================================================================
 
 
-#   Cette fois-ci, la boucle de l'algorithme est adaptée pour traiter les 
-#   figures symétriques, pour gagner du temps d'execution notamment. 
-#   On travaille d'abord avec un nombre de côtés impaires car les 
-#   mouvement sont beaucoup plus faciles à traiter qu'avec les polygones 
-#   pairs 
+#   Cette fois-ci, la boucle de l'algorithme est adaptée pour traiter les
+#   figures symétriques, pour gagner du temps d'execution notamment.
+#   On travaille d'abord avec un nombre de côtés impaires car les
+#   mouvement sont beaucoup plus faciles à traiter qu'avec les polygones
+#   pairs
 
-def OSMainloop(polygon, dl, nbIteration, nbTest, eng) : 
-    
-    if nbIteration == 0 : 
+def OSMainloop(polygon, dl, nbIteration, nbTest, eng) :
+
+    if nbIteration == 0 :
         print("Fin de la simulation")
-        return 0    
+        return 0
 
     initValue = polygon.valueIntegral(0,0,eng)
 
-    #   On cherche le petit déplacement qui maximise notre fonctionnelle 
-    #   de forme lors d'une itération de l'algorithme 
+    #   On cherche le petit déplacement qui maximise notre fonctionnelle
+    #   de forme lors d'une itération de l'algorithme
     #   On examine chacun des sommets indépendamment
 
     max = [0,0]                                                     # Initialisation du maximum
-    rank = 0 
-    for i in range(2, int((polygon.N + 3) / 2)) : 
+    rank = 0
+    for i in range(2, int((polygon.N + 3) / 2)) :
         val = bestValueOS(polygon, initValue, i, nbTest, dl, eng)
         if val[0] > max[0] :
             max = val
-            rank = i 
+            rank = i
 
     #   S'il n'y a pas d'améliorations, on stoppe
-    if max[1] == 0 :  
+    if max[1] == 0 :
         print("Fin de la simulation")
         return 1
-    
-    # Sinon on bouge un sommet 
-    polygon.move(rank, max[1]) 
+
+    # Sinon on bouge un sommet
+    polygon.move(rank, max[1])
     polygon.move(polygon.N - rank + 1, -max[1])
-        
-    # Appel récursive de la fonction 
-    OSMainloop(polygon, dl, nbIteration - 1, nbTest, eng)     
+
+    # Appel récursive de la fonction
+    OSMainloop(polygon, dl, nbIteration - 1, nbTest, eng)
 
 
 
 
-#   Idées à faire pour le developpement du code : 
+#   Idées à faire pour le developpement du code :
 #
-#   @ Exploitation des symétries 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#   @ Exploitation des symétries
--- Python/main.py
+++ Python/main.py
@@ -10,20 +10,19 @@ import time
 #   Définition de P
 v1 = Vertex(0,-1)
 v2 = Vertex(0,1)
-v3 = Vertex(1,0)
-v4 = Vertex(2,-0.5)
-v5 = Vertex(1,-1)
+v3 = Vertex(2,1.5)
+v4 = Vertex(3,0.5)
+v5 = Vertex(1,-1.5)
 P = Polygon(v1, v2, v3, v4, v5)
-
 
 
 
 #   Définition de Q Symétrique
 c1 = Vertex(0, -1)
-c2 = Vertex(0,1)
-c3 = Vertex(1,1)
-c4 = Vertex(2,0)
-c5 = Vertex(1,-1)
+c2 = Vertex(0, 1)
+c3 = Vertex(0.1, 4)
+c4 = Vertex(1.410000000000001, 0)
+c5 = Vertex(0.1, -4)
 Q = Polygon(c1, c2, c3, c4, c5)
 
 
@@ -31,24 +30,25 @@ Q = Polygon(c1, c2, c3, c4, c5)
 
 eng = matlab.engine.start_matlab()
 
+print(P.valueIntegral(0, 0, eng))
+print(P.area())
+P.plotPY('g')
+plt.show()
 # Tracé initial et stockage de la valeur de l'intégrale initiale
 
 start = time.clock()
-initMean = Q.valueIntegral(0,0,eng)             # Valeur initiale
-initArea = Q.area()
-
-
-Q.plotPY('g')
+initMean = P.valueIntegral(0,0,eng)             # Valeur initiale
+initArea = P.area()
 
 
 values = []
 # Boucle principale
-naiveMainloop(Q, 0.1, 2, 10, values, eng)
+naiveMainloop(P, 0.1, 2, 5, values, eng)
 print(values)
 
 
-endMean = Q.valueIntegral(0,0,eng)             # Valeur finale
-endArea = Q.area()
+endMean = P.valueIntegral(0,0,eng)             # Valeur finale
+endArea = P.area()
 
 print("Valeur initiale : " + str(initMean))
 print("Valeur finale : " + str(endMean))
@@ -61,5 +61,5 @@ print("\nTemps de la simulation " + str(end - start))
 
 
 # Tracé final
-Q.plotPY('b')
+P.plotPY('b')
 plt.show()
