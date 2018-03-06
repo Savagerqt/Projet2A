89071697a79b36665c84e5b415f5fe3f371bdd9a
Tanguy VIVIER <tanguy.viv@gmail.com>
2018-03-06 10:06:36 +0100

    Rapport Latex
    Ajout du rapport Latex

--- Python/main.py
+++ Python/main.py
@@ -1,53 +1,53 @@
 from Vertex import *
 from Polygon_Optimization import *
 import matplotlib.pyplot as plt
-import math as m 
-import numpy as np 
-import matlab.engine 
-import time 
+import math as m
+import numpy as np
+import matlab.engine
+import time
 
 
-#   Définition de P 
+#   Définition de P
 v1 = Vertex(0,-1)
-v2 = Vertex(0,1) 
-v3 = Vertex(1,1.5) 
-v4 = Vertex(2,-1) 
-v5 = Vertex(0.5,-2)
+v2 = Vertex(0,1)
+v3 = Vertex(1,0)
+v4 = Vertex(2,-0.5)
+v5 = Vertex(1,-1)
 P = Polygon(v1, v2, v3, v4, v5)
 
 
 
 
 #   Définition de Q Symétrique
-c1 = Vertex(0, -1) 
+c1 = Vertex(0, -1)
 c2 = Vertex(0,1)
 c3 = Vertex(1,1)
-c4 = Vertex(2,0) 
+c4 = Vertex(2,0)
 c5 = Vertex(1,-1)
 Q = Polygon(c1, c2, c3, c4, c5)
 
 
 
 
-eng = matlab.engine.start_matlab() 
+eng = matlab.engine.start_matlab()
 
-# Tracé initial et stockage de la valeur de l'intégrale initiale 
+# Tracé initial et stockage de la valeur de l'intégrale initiale
 
 start = time.clock()
 initMean = Q.valueIntegral(0,0,eng)             # Valeur initiale
 initArea = Q.area()
 
 
-Q.plotPY('g')    
+Q.plotPY('g')
 
 
 values = []
-# Boucle principale 
-OSMainloop(Q, 0.1, 10, 1, eng)
+# Boucle principale
+naiveMainloop(Q, 0.1, 2, 10, values, eng)
 print(values)
 
 
-endMean = Q.valueIntegral(0,0,eng)             # Valeur finale 
+endMean = Q.valueIntegral(0,0,eng)             # Valeur finale
 endArea = Q.area()
 
 print("Valeur initiale : " + str(initMean))
@@ -60,15 +60,6 @@ end = time.clock()
 print("\nTemps de la simulation " + str(end - start))
 
 
-# Tracé final 
+# Tracé final
 Q.plotPY('b')
 plt.show()
-
-
-'''
-y = [i for i in range(len(values))]
-plt.plot(y, values,'x')  
-'''
-
-
-
