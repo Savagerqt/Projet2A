5aad7a428ec862d8910fb071053b3c337dbc377c
Tanguy VIVIER <tanguy.viv@gmail.com>
2018-03-07 19:10:12 +0100

    Petit Update de mi-Parcours PPT

--- Python/main.py
+++ Python/main.py
@@ -7,17 +7,23 @@ import matlab.engine
 import time
 
 
+#   Définition de S
+s1 = Vertex(0,-1)
+s2 = Vertex(0,1)
+s3 = Vertex(2,1.5)
+s4 = Vertex(3,0.5)
+s5 = Vertex(1,-1.5)
+S = Polygon(s1, s2, s3, s4, s5)
+
+
 #   Définition de P
-v1 = Vertex(0,-1)
-v2 = Vertex(0,1)
-v3 = Vertex(2,1.5)
-v4 = Vertex(3,0.5)
-v5 = Vertex(1,-1.5)
+v1 = Vertex(0, -1)
+v2 = Vertex(0, 1)
+v3 = Vertex(0.2, 1)
+v4 = Vertex(5.549999999999926, 0)
+v5 = Vertex(0.2, -1)
 P = Polygon(v1, v2, v3, v4, v5)
 
-print(P.degSymetrie(0.1))
-P.plotPY('r')
-plt.show()
 
 #   Définition de Q Symétrique
 c1 = Vertex(0, -1)
@@ -27,13 +33,27 @@ c4 = Vertex(1.410000000000001, 0)
 c5 = Vertex(0.1, -4)
 Q = Polygon(c1, c2, c3, c4, c5)
 
+r1 = Vertex(0, -1)
+r2 = Vertex(0, 1)
+r3 = Vertex(0.05, 1.3)
+r4 = Vertex(4.379999999999951, 0)
+r5 = Vertex(0.05, -1.3)
+R = Polygon(r1, r2, r3, r4, r5)
 
+
+
+eng = matlab.engine.start_matlab()
+print(S.valueIntegral(0, 0, eng))
+
+
+
+
+plt.show()
 
 
 
 '''
 
-eng = matlab.engine.start_matlab()
 
 print(P.valueIntegral(0, 0, eng))
 print(P.area())
@@ -68,8 +88,5 @@ print("\nTemps de la simulation " + str(end - start))
 # Tracé final
 P.plotPY('b')
 plt.show()
-
-
-
 
 '''
